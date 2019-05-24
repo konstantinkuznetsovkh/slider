f3690da12dd6f2591ee5042f46e89aa99cb9224f
LazyCoder <konstantinkuznetsov.kh@gmail.com>
2019-05-24 04:31:16 +0100

    24052019043012

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-var links = document.querySelectorAll(".itemLinks");
-var slider = document.querySelectorAll(".slContent");
-var wrapper = document.querySelector(".slWrap");
+var links = document.querySelectorAll('.itemLinks');
+var slider = document.querySelectorAll('.slContent');
+var wrapper = document.querySelector('.slWrap');
 var prev = document.querySelector('.left');
 var next = document.querySelector('.right');
 var left = 0;
@@ -11,17 +11,16 @@ for (var i = 0; i < links.length; i++) {
 	var link = links[i];
 	link.addEventListener('click', setClickedItem, false);
 	link.itemID = i;
-
 }
 //всплытие с event индекс
 var activeLink;
 prev.addEventListener('click', function showPrev() {
 	activeLink = (activeLink - 1) % slider.length;
-	var widthSlContent = document.getElementsByClassName("img");
+	var widthSlContent = document.getElementsByClassName('img');
 	// var positionInfo = widthSlContent[activeLink].getBoundingClientRect();
 	var positionInfo = widthSlContent[0].getBoundingClientRect();
 	var width = positionInfo.width;
-	var position = (width - 133 - 26);
+	var position = width - 133 - 26;
 	var positionInfoWrapper = wrapper.getBoundingClientRect();
 	possitionWrapper = positionInfoWrapper.left;
 	console.log(possitionWrapper);
@@ -30,26 +29,23 @@ prev.addEventListener('click', function showPrev() {
 	if (activeLink <= 0) {
 		activeLink = 0;
 		wrapper.style.left = 0;
-		slider[0].classList.add("activeSl");
-		links[0].classList.add("active");
+		slider[0].classList.add('activeSl');
+		links[0].classList.add('active');
 	} else {
-
-		leftW = (-possitionWrapper) - position;
+		leftW = -possitionWrapper - position;
 		wrapper.style.left = -leftW + 'px';
-		slider[activeLink].classList.add("activeSl");
-		links[activeLink].classList.add("active");
+		slider[activeLink].classList.add('activeSl');
+		links[activeLink].classList.add('active');
 	}
-
 });
 
 next.addEventListener('click', function showNext() {
-
 	activeLink = (activeLink + 1) % slider.length;
-	var widthSlContent = document.getElementsByClassName("img");
+	var widthSlContent = document.getElementsByClassName('img');
 	// var positionInfo = widthSlContent[activeLink].getBoundingClientRect();
 	var positionInfo = widthSlContent[0].getBoundingClientRect();
 	var width = positionInfo.width;
-	var position = (width + 133 + 292);
+	var position = width + 133 + 292;
 	var positionInfoWrapper = wrapper.getBoundingClientRect();
 	possitionWrapper = positionInfoWrapper.left;
 	console.log(possitionWrapper);
@@ -58,20 +54,18 @@ next.addEventListener('click', function showNext() {
 		activeLink = 0;
 		wrapper.style.left = 0;
 		console.log(wrapper.style.left);
-		slider[0].classList.add("activeSl");
-		links[0].classList.add("active");
+		slider[0].classList.add('activeSl');
+		links[0].classList.add('active');
 	} else {
-
 		leftW = possitionWrapper - position;
 		wrapper.style.left = leftW + 'px';
-		slider[activeLink].classList.add("activeSl");
-		links[activeLink].classList.add("active");
+		slider[activeLink].classList.add('activeSl');
+		links[activeLink].classList.add('active');
 	}
-
 });
 
-links[activeLink].classList.add("active");
-slider[activeLink].classList.add("activeSl");
+links[activeLink].classList.add('active');
+slider[activeLink].classList.add('activeSl');
 
 function setClickedItem(e) {
 	removeActiveLinks();
@@ -83,22 +77,22 @@ function setClickedItem(e) {
 
 function removeActiveLinks() {
 	for (var i = 0; i < links.length; i++) {
-		links[i].classList.remove("active");
+		links[i].classList.remove('active');
 	}
 	for (var i = 0; i < slider.length; i++) {
-		slider[i].classList.remove("activeSl");
+		slider[i].classList.remove('activeSl');
 	}
 }
 
 var position;
 
 function changePosition(link) {
-	var widthSlContent = document.getElementsByClassName("slContent");
+	var widthSlContent = document.getElementsByClassName('slContent');
 	var positionInfo = widthSlContent[widthSlContent.length - 1].getBoundingClientRect();
-	link.classList.add("active");
+	link.classList.add('active');
 
-	slider[activeLink].classList.add("activeSl");
+	slider[activeLink].classList.add('activeSl');
 	var width = positionInfo.width;
-	var position = ((width + 133) * -activeLink) + "px";
+	var position = (width + 133) * -activeLink + 'px';
 	wrapper.style.left = position;
 }
